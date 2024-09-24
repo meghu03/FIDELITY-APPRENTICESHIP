84bba4da6fd72816a91bb30f734ede218c0393c0
Meghana S <116013635+meghu03@users.noreply.github.com>
2024-09-24 17:59:17 +0530

    Day1 assignment

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,62 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Multilevel inheritance
-        Smartphone smartphone = new Smartphone("iphone 15", 23000, "APPLE", "iphone");
-        System.out.println("-----Smartphone Details-----:");
-        smartphone.displayInfo();
+        Scanner scanner = new Scanner(System.in);
+        //Input
+        System.out.print("Enter student's marks");
+        double marks = scanner.nextDouble();
+        if (marks < 0 || marks > 100) {
+            System.out.println("Invalid marks");
+            return;
+        }
 
-        System.out.println();
+        String grade;
+        if (marks >= 90) {
+            grade = "A";
+        } else if (marks >= 80) {
+            grade = "B";
+        } else if (marks >= 70) {
+            grade = "C";
+        } else if (marks >= 60) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+        System.out.println("Student's marks: " + marks);
+        System.out.println("Grade:" + grade);
 
-        // Hierarchical inheritance
-        Appliances television = new Appliances("Crystal", 50000, "Samsung");
-        System.out.println("-----Appliances Details-----:");
-        television.displayInfo();
-
-        Clothing tShirt = new Clothing("T-Shirt", 800, "M");
-        System.out.println("-----Clothing Details-----:");
-        tShirt.displayInfo();
+        String feedback;
+        switch(grade) {
+            case "A":
+                feedback = "Excellent";
+                break;
+            case "B":
+                feedback = "Good";
+                break;
+            case "C":
+                feedback = "Satisfactory";
+                break;
+            case "D":
+                feedback = "Needs improvement";
+                break;
+            case "F":
+                feedback = "Fail";
+                break;
+            default:
+                feedback = "Invalid grade";
+                System.out.println("Feedback: "+feedback);
+                scanner.close();
+        }
     }
 }
+
+
+
+
+
+
+
+
