5940b58c2fc6ad39132fa57ef2319b416384b793
Meghana S <116013635+meghu03@users.noreply.github.com>
2024-09-25 10:19:20 +0530

    Day2 Assignment

---
+++ Day2/src/Product.java
@@ -0,0 +1,15 @@
+public class Product {
+    String name;
+    double price;
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    public void displayInfo() {
+        System.out.println("Product Name: " + name + "\nPrice: " + price);
+    }
+}
+
+
+
+
